3e978fe522c0f3a30ea5b333a6f867cd562a646a
denish4848 <denishsavaliya.elsner@gmail.com>
2022-02-17 14:56:00 +0530

    changes

--- src/component/AdminPage/AdminLogin.jsx
+++ src/component/AdminPage/AdminLogin.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import "./AdminPage.css";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import MovieLinkData from "../MovieData/MovieLinkData";
 const AdminLogin = () => {
   //for redirect component:
-  const redirect = useNavigate();
+  // const redirect = useNavigate();
   const [incPassword, setIncPassword] = useState("");
+  const [render_comp, setRender_comp] = useState(false);
 
   //usestate for get value on change:
   const [user, setUser] = useState({
     password: "",
+    email: "",
   });
 
   let name, value;
@@ -16,11 +19,11 @@ const AdminLogin = () => {
   const pass_UserData = async (e) => {
     name = e.target.name;
     value = e.target.value;
-   await setUser({ ...user, [name]: value });
+    await setUser({ ...user, [name]: value });
   };
 
   //subit handler function:
-  const submit_getData = (e) => {
+  const submit_Data = (e) => {
     e.preventDefault();
     console.log("user", user);
     //check the condition for password match and redirect to other component:
@@ -28,56 +31,63 @@ const AdminLogin = () => {
     //   ? redirect("/mdata")
     //   : setIncPassword("Incorrect Password");
 
-    if (user?.password === "123") {
-      redirect("/mdata");
+    if (user?.password === "123" && user?.email === "denish@gmail.com") {
+      setRender_comp(true);
     }
     if (user?.password === "") {
       setIncPassword("Enter Password");
+    }
+    if (user?.email === "") {
+      setIncPassword("Enter Email");
     } else {
-      setIncPassword("Incorrecct password");
+      setIncPassword("Incorrecct Password or Email");
     }
   };
 
   return (
     <>
-      <div>
-        <div className="login-box">
-          <h2>Login</h2>
-          <form>
-            <div className="user-box">
-              <input
-                type="text"
-                name="email"
-                id="email"
-                value="denish@gmail.com"
-                autoComplete="off"
-              />
-              <label>Email</label>
-            </div>
-            <div className="user-box">
-              <input
-                required
-                type="password"
-                name="password"
-                id="password"
-                value={user.password}
-                onChange={pass_UserData}
-                autoComplete="off"
-              />
-              <label>Password</label>
-            </div>
-            <a href="#" onClick={submit_getData}>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              Submit
-            </a>
-          </form>
-          <br />
-          <h4 style={{ color: "skyblue" }}>{incPassword}</h4>
+      {!render_comp && (
+        <div>
+          <div className="login-box">
+            <h2>Login</h2>
+            <form>
+              <div className="user-box">
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={user.email}
+                  onChange={pass_UserData}
+                  autoComplete="off"
+                />
+                <label>Email</label>
+              </div>
+              <div className="user-box">
+                <input
+                  required
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={user.password}
+                  onChange={pass_UserData}
+                  autoComplete="off"
+                />
+                <label>Password</label>
+              </div>
+              <a href="/mdata" onClick={submit_Data}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+              </a>
+            </form>
+            <br />
+            <h4 style={{ color: "skyblue" }}>{incPassword}</h4>
+          </div>
         </div>
-      </div>
+      )}
+      {render_comp && <MovieLinkData />}
     </>
   );
 };
