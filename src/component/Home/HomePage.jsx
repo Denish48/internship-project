f32b3b896d27f537d4e77a86e806e24d8c0be838
denish4848 <denishsavaliya.elsner@gmail.com>
2022-02-17 19:38:59 +0530

    changes

--- src/component/Home/HomePage.jsx
+++ src/component/Home/HomePage.jsx
@@ -6,13 +6,16 @@ const HomePage = () => {
   return (
     <>
       <h1>Movie List</h1>
-      <div>
-        <img src="https://m.media-amazon.com/images/M/MV5BMzUxNGIyY2MtNDUxZC00YWY1LTg0MzctM2ExNTNmODlhMTRlXkEyXkFqcGdeQXVyMTE4Nzk0MzE4._V1_SX300.jpg" alt="" />
+      <div className="main_div">
+        <div className="image_set_div">
+          <img src="https://m.media-amazon.com/images/M/MV5BMzUxNGIyY2MtNDUxZC00YWY1LTg0MzctM2ExNTNmODlhMTRlXkEyXkFqcGdeQXVyMTE4Nzk0MzE4._V1_SX300.jpg" alt="" />
+        </div>
+        <div className="download_link_div">
+          <a href="#">480P</a><br />
+          <a href="#">720P</a>
+        </div>
       </div>
-      <div>
-        <a href="">480P</a><br />
-        <a href="">720P</a>
-      </div>
+
     </>
   );
 };
