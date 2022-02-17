df7b8f7baac8f1552cd71fc64b3f00f14e4008f9
denish4848 <denishsavaliya.elsner@gmail.com>
2022-02-17 10:38:24 +0530

    change

--- src/component/AdminPage/AdminLogin.jsx
+++ src/component/AdminPage/AdminLogin.jsx
@@ -1,58 +1,65 @@
 import React, { useState } from "react";
 import "./AdminPage.css";
-import Avatar from "@mui/material/Avatar";
 
 const AdminLogin = () => {
+  //usestate for get value on change:
   const [user, setUser] = useState({
-    email: "",
     password: "",
   });
 
   let name, value;
-  const UserData = (e) => {
+  //value handler function:
+  const pass_UserData = (e) => {
     name = e.target.name;
     value = e.target.value;
     setUser({ ...user, [name]: value });
   };
-  console.log(user);
-  const getData = (e) => {
+
+  //subit handler function:
+  const submit_getData = (e) => {
     e.preventDefault();
-    setUser({ email: "", password: "" });
+    console.log("user", user);
+    //check the condition for password match:
+    user?.password === "123456" ? alert("Sucesss") : alert("try again");
   };
+
   return (
     <>
       <div>
-        <form action="submit" method="POST">
-          <div className="login_avatar">
-            <Avatar src="/broken-image.jpg" />
-          </div>
-          <br />
-          <label htmlFor="Email">Email:</label>
-          <br />
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={user.email}
-            onChange={UserData}
-          />
-          <br />
-          <br />
-          <label htmlFor="Password">Enter Password:</label>
-          <br />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={user.password}
-            onChange={UserData}
-          />
-          <br />
-          <br />
-          <button className="login_button" onClick={getData}>
-            Login
-          </button>
-        </form>
+        <div className="login-box">
+          <h2>Login</h2>
+          <form>
+            <div className="user-box">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value="denish@gmail.com"
+                autoComplete="off"
+              />
+              <label>Email</label>
+            </div>
+            <div className="user-box">
+              <input
+                required
+                type="password"
+                name="password"
+                id="password"
+                value={user.password}
+                onChange={pass_UserData}
+                autoComplete="off"
+              />
+              <label>Password</label>
+            </div>
+            <a href="#" onClick={submit_getData}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Submit
+            </a>
+          </form>
+        </div>
       </div>
     </>
   );
