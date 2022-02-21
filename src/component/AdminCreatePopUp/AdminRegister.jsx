3f83eff65bcccd8e17fa98986b5245a84e2b0840
denish4848 <denishsavaliya.elsner@gmail.com>
2022-02-21 10:26:31 +0530

    chab

--- src/component/AdminCreatePopUp/AdminRegister.jsx
+++ src/component/AdminCreatePopUp/AdminRegister.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const AdminRegister = () => {
-  return (
-    <>
-      <button className="btn btn-success"  >Add New Admin</button>
-    </>
-  );
+    const [add_page_Render, setAdd_page_Render] = useState(false)
+
+    const show_form = () => {
+
+        setAdd_page_Render(true)
+
+    }
+
+    return (
+        <>
+            {
+                !add_page_Render && (
+
+                    <button className="btn btn-success" onClick={show_form}>Add New Admin</button>
+                )
+            }
+            {
+                add_page_Render && (<h1>hello</h1>)
+            }
+        </>
+    );
 };
 
 export default AdminRegister;
