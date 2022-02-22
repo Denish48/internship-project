4ee9867790f45d0c16a5d5b36eca1b5a586a6607
denish4848 <denishsavaliya.elsner@gmail.com>
2022-02-22 16:55:52 +0530

    change

--- src/component/ShowAdminData/AdminAllData.js
+++ src/component/ShowAdminData/AdminAllData.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { dt } from "../FireBase";
 import { collection, getDocs } from "firebase/firestore";
 import "./AdminAllData.css";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const AdminAllData = () => {
   const [admin_data_show, setAdmin_data_show] = useState([]);
@@ -27,25 +28,39 @@ const AdminAllData = () => {
           <table className="styled-table">
             <thead>
               <tr>
-                <th>username</th>
-                <th>email</th>
-                <th>password</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Delete Admin</th>
               </tr>
             </thead>
             {admin_data_show.map((value, ind) => {
               return (
                 <>
                   <tbody>
-                    <tr>
-                      <td>{value.user_name}</td>
-                      <td>{value.email}</td>
-                      <td>{value.password}</td>
-                    </tr>
-                    <tr className="active-row">
-                      <td>{value.user_name}</td>
-                      <td>{value.email}</td>
-                      <td>{value.password}</td>
-                    </tr>
+                    {ind % 2 === 0 ? (
+                      <tr>
+                        <td>{value.user_name}</td>
+                        <td>{value.email}</td>
+                        <td>{value.password}</td>
+                        <td>
+                          <button className="btn btn-danger">
+                            <DeleteForeverIcon />
+                          </button>
+                        </td>
+                      </tr>
+                    ) : (
+                      <tr className="active-row">
+                        <td>{value.user_name}</td>
+                        <td>{value.email}</td>
+                        <td>{value.password}</td>
+                        <td>
+                          <button className="btn btn-danger">
+                            <DeleteForeverIcon />
+                          </button>
+                        </td>
+                      </tr>
+                    )}
                   </tbody>
                 </>
               );
