b03c6a05856fcf33162292e45e8a9e2f948b7d7d
denish4848 <denishsavaliya.elsner@gmail.com>
2022-02-28 19:24:35 +0530

    chnage

--- src/component/wallpaper/WallpaperDataShow.js
+++ src/component/wallpaper/WallpaperDataShow.js
@@ -2,28 +2,27 @@ import UnsplashReact, {
   Base64Uploader,
   withDefaultProps,
 } from "unsplash-react";
-import "./WallpaperDataShow.css"
+import "./WallpaperDataShow.css";
 
 const MY_ACCESS_KEY = "WqDpKTnRaRLgHVyNOExPGeSFVdujHOpYhl4jWJ2w3L4";
 const MY_SECRET_KEY = "wl4gRWE6FgygtlF9koseXM5_Yr1Ta_cipZ8WV2zkeTw";
-function WallpaperDataShow() {
-  const loadings = () => {
-    alert("not responce API");
-  };
+const COLOR = "#00000";
 
+function WallpaperDataShow() {
+  
   return (
     <>
       <div className="App">
-      <div className="wallpaper_img">
-        <UnsplashReact
-          photoRatio={7 / 12}
-          onFinishedUploading={loadings}
-          secretkey={MY_SECRET_KEY}
-          accessKey={MY_ACCESS_KEY}
-          columns={8}
-          Uploader={withDefaultProps(Base64Uploader, { name: "event[logo]" })}
-        />
-      </div>
+        <div className="main_wall">
+          <UnsplashReact
+            highlightColor={COLOR}
+            photoRatio={10 / 18}
+            secretkey={MY_SECRET_KEY}
+            accessKey={MY_ACCESS_KEY}
+            columns={7}
+            Uploader={withDefaultProps(Base64Uploader, { name: "event[logo]" })}
+          />
+        </div>
       </div>
     </>
   );
